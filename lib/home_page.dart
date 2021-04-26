e644e919df92fa641ee75f3671adaf5a05b7b951
Eduardo Garcia Laguna <dev.lalo.desarrollo@gmail.com>
2021-04-25 23:37:39 -0500

    manecillas para hora y minuto

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -84,6 +84,24 @@ class MyClockPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
     final path = new Path();
+
+    //
+    // Rotation of all widget
+    //
+
+    var angle = Vector.radians(-90);
+    final double r =
+        sqrt(size.width * size.width + size.height * size.height) / 2;
+    final alpha = atan(size.height / size.width);
+    final beta = alpha + angle;
+    final shiftY = r * sin(beta);
+    final shiftX = r * cos(beta);
+    final translateX = size.width / 2 - shiftX;
+    final translateY = size.height / 2 - shiftY;
+    canvas.translate(translateX, translateY);
+    canvas.rotate(angle);
+
+    DateTime now = DateTime.now();
     path.moveTo(size.width * .5, 25);
     path.lineTo(size.width * 0.5, 45);
     path.moveTo(size.width - 25, size.height * .5);
@@ -93,15 +111,74 @@ class MyClockPainter extends CustomPainter {
     path.moveTo(25, size.height * .5);
     path.lineTo(45, size.height * .5);
     canvas.drawPath(path, paint);
-    DateTime now = DateTime.now();
+
+    //Center of clock
     var center = Offset(size.width / 2, size.height / 2);
-    paint.color = Colors.white;
-    paint.strokeCap = StrokeCap.round;
 
     /**
-     * Seconds line
+     * Hour line and pre line
      */
+    final hourP1 = center;
+
+    double hourDegree = 360 / 12 * now.hour;
+    // x = cx + r * cos(a)
+    x = (size.width / 2) + (size.width / 3) * cos(Vector.radians(hourDegree));
+    // y = cy + r * sin(a)
+    y = (size.height / 2) + (size.width / 3) * sin(Vector.radians(hourDegree));
+
+    final hourP2 = Offset(x, y);
+
+    paint.color = Colors.black;
+
+    paint.strokeWidth = 2;
+    canvas.drawLine(hourP1, hourP2, paint);
+
+    double xSub = (size.width / 2) +
+        (-size.width / 3 + 95) * cos(Vector.radians(hourDegree));
+    double ySub = (size.height / 2) +
+        (-size.width / 3 + 95) * sin(Vector.radians(hourDegree));
+    final hourPSub = Offset(xSub, ySub);
+    canvas.drawLine(hourP1, hourPSub, paint);
+    /**
+     * End Hour line and pre line
+     */
+
+    /**
+     * Minute line and pre line
+     */
+    final minutesP1 = center;
+
+    double minutesDegree = 360 / 60 * now.minute;
+    // x = cx + r * cos(a)
+    x = (size.width / 2) +
+        (size.width / 3 + 15) * cos(Vector.radians(minutesDegree));
+    // y = cy + r * sin(a)
+    y = (size.height / 2) +
+        (size.width / 3 + 15) * sin(Vector.radians(minutesDegree));
+
+    final minutesP2 = Offset(x, y);
+
+    paint.color = Colors.black;
+
+    paint.strokeWidth = 2;
+    canvas.drawLine(minutesP1, minutesP2, paint);
+
+    xSub = (size.width / 2) +
+        (-size.width / 3 + 95) * cos(Vector.radians(minutesDegree));
+    ySub = (size.height / 2) +
+        (-size.width / 3 + 95) * sin(Vector.radians(minutesDegree));
+    final minutesPSub = Offset(xSub, ySub);
+    canvas.drawLine(minutesP1, minutesPSub, paint);
+    /**
+     * End Minute line and pre line
+     */
+
+    /**
+     * Seconds line and pre line
+     */
+
     final secondsP1 = center;
+
     double secondDegree = 360 / 60 * now.second;
     // x = cx + r * cos(a)
     double x = (size.width / 2) +
@@ -116,18 +193,23 @@ class MyClockPainter extends CustomPainter {
 
     paint.strokeWidth = 2;
     canvas.drawLine(secondsP1, secondsP2, paint);
-    double xSub = (size.width / 2) +
-        (-size.width / 3 + 92) * cos(Vector.radians(secondDegree));
-    // y = cy + r * sin(a)
-    double ySub = (size.height / 2) +
-        (-size.width / 3 + 92) * sin(Vector.radians(secondDegree));
+    xSub = (size.width / 2) +
+        (-size.width / 3 + 95) * cos(Vector.radians(secondDegree));
+    ySub = (size.height / 2) +
+        (-size.width / 3 + 95) * sin(Vector.radians(secondDegree));
     final secondsPSub = Offset(xSub, ySub);
     canvas.drawLine(secondsP1, secondsPSub, paint);
+    /**
+     * End of seconds line
+     */
 
+    /**
+     * Circle of center
+     */
     paint.color = Colors.white;
     paint.strokeWidth = 0;
     paint.style = PaintingStyle.fill;
-    canvas.drawCircle(center, 4, paint);
+    canvas.drawCircle(center, 2, paint);
   }
 
   @override
